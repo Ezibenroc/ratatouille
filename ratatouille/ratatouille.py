Ezibenroc/ratatouille

commit 5299e0ecf9b3808f339c61a5c5438ed50aa39a0a

Fix the Temperature monitor when several values exist for a same key

ratatouille/ratatouille.py
@@ -90,22 +90,8 @@ class Temperature(AbstractWatcher):
     reg = re.compile('Core (?P<id>[0-9]+)')
 
     def __init__(self):
+        self.header = list(self._get_values_dict().keys())
         super().__init__()
-        self.header = self.build_header()
-
-    def build_header(self):
-        temperatures = psutil.sensors_temperatures()
-        coretemps = self._get_core_temps(temperatures)
-        header = ['temperature_core_%d' % i for i in range(len(coretemps))]
-        for key, value in sorted(temperatures.items()):
-            if key == 'coretemp':
-                continue
-            assert len(value) == 1
-            label = value[0].label
-            if label != '':
-                key = '%s_%s' % (key, label)
-            header.append(key)
-        return header
 
     @classmethod
     def _get_core_temps(cls, temperatures):
@@ -116,17 +102,22 @@ class Temperature(AbstractWatcher):
             if match:
                 values.append((int(match.group('id')), temp.current))
         values.sort(key = lambda t: t[0])
-        return [t[1] for t in values]
+        return {'temperature_core_%d' % t[0]: t[1] for t in values}
 
-    def get_values(self):
+    @classmethod
+    def _get_values_dict(cls):
         temperatures = psutil.sensors_temperatures()
-        alltemps = self._get_core_temps(temperatures)
+        alltemps = cls._get_core_temps(temperatures)
         for key, value in sorted(temperatures.items()):
             if key == 'coretemp':
                 continue
-            assert len(value) == 1
-            alltemps.append(value[0].current)
+            for elt in value:
+                alltemps['temperature_%s%s' % (key, elt.label)] = elt.current
         return alltemps
+
+    def get_values(self):
+        values = self._get_values_dict()
+        return [values[k] for k in self.header]
 
 
 class Network(AbstractWatcher):

commit 1a93c5e6882d7470cb833f09827e9bb4bd3633f6

Fix the temperature monitoring
Bug introduced by commit 5299e0ecf9b3808f339c61a5c5438ed50aa39a0a

For multi-socket machines, only the core temperatures of the last socket were displayed.
The reason is that psutil gives a same label to cores from a distinct
socket.

ratatouille/ratatouille.py
@@ -87,7 +87,7 @@ class MemoryUsage(AbstractWatcher):
 
 
 class Temperature(AbstractWatcher):
-    reg = re.compile('Core (?P<id>[0-9]+)')
+    reg = re.compile('Core (?P<core_id>[0-9]+)|Package id (?P<package_id>[0-9]+)')
 
     def __init__(self):
         self.header = list(self._get_values_dict().keys())
@@ -96,13 +96,33 @@ class Temperature(AbstractWatcher):
     @classmethod
     def _get_core_temps(cls, temperatures):
         coretemps = temperatures.get('coretemp', [])
-        values = []
+        packages = set()
+        # First, we iterate on the labels to get all the packages ID
         for temp in coretemps:
             match = cls.reg.match(temp.label)
-            if match:
-                values.append((int(match.group('id')), temp.current))
-        values.sort(key = lambda t: t[0])
-        return {'temperature_core_%d' % t[0]: t[1] for t in values}
+            assert match is not None
+            package_id = match.group('package_id')
+            if package_id is not None:
+                package_id = int(package_id)
+                assert package_id not in packages
+                packages.add(package_id)
+        # If there are 4 distinct packages, we expect them to be labelled as 0,1,2,3
+        nb_packages = len(packages)
+        assert packages == set(range(nb_packages))
+        # Finally, we iterate a second time to build the list of core temperatures
+        package_id = -1
+        values = {}
+        for temp in coretemps:
+            match = cls.reg.match(temp.label)
+            if match.group('package_id') is not None:
+                package_id = int(match.group('package_id'))
+            else:
+                core_id = match.group('core_id')
+                assert core_id is not None
+                core_id = int(core_id)
+                core_id = core_id*nb_packages + package_id
+                values['temperature_core_%d' % core_id] = temp.current
+        return values
 
     @classmethod
     def _get_values_dict(cls):