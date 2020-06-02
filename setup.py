903ce01f6d4acc120b2daad1045f11d860268093
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-06-02 18:38:51 +0200

    Stop using the latest psutil version (5.7.0)
    On some machines, the temperature entries are duplicated, see this
    issue: https://github.com/giampaolo/psutil/issues/1708

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
                                   ]
           },
           install_requires=[
-              'psutil',
+              'psutil<5.7.0',
               'pandas',
           ],
           url='https://github.com/Ezibenroc/ratatouille',
