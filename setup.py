4447f4fbf637817cde0d4b4b3b8c460aa03a233a
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2022-01-18 21:07:17 +0100

    Support Python 3.9

--- setup.py
+++ setup.py
@@ -72,5 +72,6 @@ if __name__ == '__main__':
               'Operating System :: POSIX :: Linux',
               'Programming Language :: Python :: 3.7',
               'Programming Language :: Python :: 3.8',
+              'Programming Language :: Python :: 3.9',
           ],
           )
